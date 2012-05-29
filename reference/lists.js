b390858e92d75dbfcd59f6dd45c04c797091c793
schauer.gregor <schauer.gregor@f845d26e-ca1b-6a14-9f41-d569aa8e2ed7>
2012-05-29 08:46:17 +0000

    Javadoc

---
+++ reference/lists.js
@@ -0,0 +1,18 @@
+var DATA = [
+      { id:0, label:"at.schauer.gregor.dormancy", link:"at/schauer/gregor/dormancy/package-summary.html", type:"package" },
+      { id:1, label:"at.schauer.gregor.dormancy.Dormancy", link:"at/schauer/gregor/dormancy/Dormancy.html", type:"class" },
+      { id:2, label:"at.schauer.gregor.dormancy.EntityPersisterConfiguration", link:"at/schauer/gregor/dormancy/EntityPersisterConfiguration.html", type:"class" },
+      { id:3, label:"at.schauer.gregor.dormancy.interceptor", link:"at/schauer/gregor/dormancy/interceptor/package-summary.html", type:"package" },
+      { id:4, label:"at.schauer.gregor.dormancy.interceptor.DormancyInterceptor", link:"at/schauer/gregor/dormancy/interceptor/DormancyInterceptor.html", type:"class" },
+      { id:5, label:"at.schauer.gregor.dormancy.interceptor.PersistenceEndpoint", link:"at/schauer/gregor/dormancy/interceptor/PersistenceEndpoint.html", type:"class" },
+      { id:6, label:"at.schauer.gregor.dormancy.persister", link:"at/schauer/gregor/dormancy/persister/package-summary.html", type:"package" },
+      { id:7, label:"at.schauer.gregor.dormancy.persister.AbstractContainerPersister", link:"at/schauer/gregor/dormancy/persister/AbstractContainerPersister.html", type:"class" },
+      { id:8, label:"at.schauer.gregor.dormancy.persister.AbstractEntityPersister", link:"at/schauer/gregor/dormancy/persister/AbstractEntityPersister.html", type:"class" },
+      { id:9, label:"at.schauer.gregor.dormancy.persister.CollectionPersister", link:"at/schauer/gregor/dormancy/persister/CollectionPersister.html", type:"class" },
+      { id:10, label:"at.schauer.gregor.dormancy.persister.EntityPersister", link:"at/schauer/gregor/dormancy/persister/EntityPersister.html", type:"class" },
+      { id:11, label:"at.schauer.gregor.dormancy.persister.MapPersister", link:"at/schauer/gregor/dormancy/persister/MapPersister.html", type:"class" },
+      { id:12, label:"at.schauer.gregor.dormancy.persister.NoOpEntityPersister", link:"at/schauer/gregor/dormancy/persister/NoOpEntityPersister.html", type:"class" },
+      { id:13, label:"at.schauer.gregor.dormancy.util", link:"at/schauer/gregor/dormancy/util/package-summary.html", type:"package" },
+      { id:14, label:"at.schauer.gregor.dormancy.util.DormancyUtils", link:"at/schauer/gregor/dormancy/util/DormancyUtils.html", type:"class" }
+
+    ];
