5ef53b464861e43faac7156a96d06bf3fd469bfe
schauer.gregor <schauer.gregor@f845d26e-ca1b-6a14-9f41-d569aa8e2ed7>
2012-06-22 21:45:13 +0000

    updated license

--- core/src/main/java/at/schauer/gregor/dormancy/EntityPersisterConfiguration.java
+++ core/src/main/java/at/schauer/gregor/dormancy/EntityPersisterConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Gregor Schauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.schauer.gregor.dormancy;
 
 import javax.annotation.Nonnull;
@@ -32,8 +47,8 @@ public class EntityPersisterConfiguration {
 
 	public EntityPersisterConfiguration() {
 		this.deleteRemovedEntities = false;
-		this.saveAssociationsProperties = true;
-		this.saveNewEntities = true;
+		this.saveAssociationsProperties = false;
+		this.saveNewEntities = false;
 		this.versionChecking = true;
 	}
 
--- hibernate3/src/main/java/at/schauer/gregor/dormancy/util/DormancyUtils.java
+++ hibernate3/src/main/java/at/schauer/gregor/dormancy/util/DormancyUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Gregor Schauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.schauer.gregor.dormancy.util;
 
 import org.hibernate.EntityMode;
@@ -41,10 +56,10 @@ public class DormancyUtils extends AbstractDormancyUtils {
 	public void setPropertyValue(@Nullable ClassMetadata metadata, @Nullable PropertyAccessor propertyAccessor, @Nonnull Object bean, @Nonnull String propertyName, @Nullable Object value) {
 		Assert.isTrue(metadata != null || propertyAccessor != null, "ClassMetadata and PropertyAccessor cannot both be null");
 		if (propertyAccessor != null) {
-			// Use Spring´s PropertyAccessor to set the value directly
+			// Use Spring's PropertyAccessor to set the value directly
 			propertyAccessor.setPropertyValue(propertyName, value);
 		} else {
-			// If no PropertyAccessor is available, use Hibernate´s ClassMetadata to set the value
+			// If no PropertyAccessor is available, use Hibernate's ClassMetadata to set the value
 			metadata.setPropertyValue(bean, propertyName, value, EntityMode.POJO);
 		}
 	}
