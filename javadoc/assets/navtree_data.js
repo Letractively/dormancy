5944932019774a648ca9f7b10a31e168c833beb5
schauer.gregor <schauer.gregor@f845d26e-ca1b-6a14-9f41-d569aa8e2ed7>
2012-06-23 16:57:07 +0000

    Javadoc

---
+++ javadoc/assets/navtree_data.js
@@ -0,0 +1,18 @@
+var NAVTREE_DATA =
+[ [ "at.schauer.gregor.dormancy", "at/schauer/gregor/dormancy/package-summary.html", [ [ "Classes", null, [ [ "Dormancy", "at/schauer/gregor/dormancy/Dormancy.html", null, "" ], [ "EntityPersisterConfiguration", "at/schauer/gregor/dormancy/EntityPersisterConfiguration.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "at.schauer.gregor.dormancy.closure", "at/schauer/gregor/dormancy/closure/package-summary.html", [ [ "Classes", null, [ [ "CollectionClosure", "at/schauer/gregor/dormancy/closure/CollectionClosure.html", null, "" ], [ "DelegateClosure", "at/schauer/gregor/dormancy/closure/DelegateClosure.html", null, "" ], [ "DormancyCloneClosure", "at/schauer/gregor/dormancy/closure/DormancyCloneClosure.html", null, "" ], [ "DormancyClosure", "at/schauer/gregor/dormancy/closure/DormancyClosure.html", null, "" ], [ "DormancyMergeClosure", "at/schauer/gregor/dormancy/closure/DormancyMergeClosure.html", null, "" ], [ "ListClosure", "at/schauer/gregor/dormancy/closure/ListClosure.html", null, "" ], [ "ResultClosure", "at/schauer/gregor/dormancy/closure/ResultClosure.html", null, "" ], [ "SetClosure", "at/schauer/gregor/dormancy/closure/SetClosure.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "at.schauer.gregor.dormancy.interceptor", "at/schauer/gregor/dormancy/interceptor/package-summary.html", [ [ "Classes", null, [ [ "DormancyAdvisor", "at/schauer/gregor/dormancy/interceptor/DormancyAdvisor.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "PersistenceEndpoint", "at/schauer/gregor/dormancy/interceptor/PersistenceEndpoint.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "DormancyAdvisor.Mode", "at/schauer/gregor/dormancy/interceptor/DormancyAdvisor.Mode.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "at.schauer.gregor.dormancy.persister", "at/schauer/gregor/dormancy/persister/package-summary.html", [ [ "Interfaces", null, [ [ "EntityPersister", "at/schauer/gregor/dormancy/persister/EntityPersister.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "AbstractContainerPersister", "at/schauer/gregor/dormancy/persister/AbstractContainerPersister.html", null, "" ], [ "AbstractEntityPersister", "at/schauer/gregor/dormancy/persister/AbstractEntityPersister.html", null, "" ], [ "CollectionPersister", "at/schauer/gregor/dormancy/persister/CollectionPersister.html", null, "" ], [ "FunctionPersister", "at/schauer/gregor/dormancy/persister/FunctionPersister.html", null, "" ], [ "MapPersister", "at/schauer/gregor/dormancy/persister/MapPersister.html", null, "" ], [ "NoOpEntityPersister", "at/schauer/gregor/dormancy/persister/NoOpEntityPersister.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "at.schauer.gregor.dormancy.util", "at/schauer/gregor/dormancy/util/package-summary.html", [ [ "Classes", null, [ [ "AbstractDormancyUtils", "at/schauer/gregor/dormancy/util/AbstractDormancyUtils.html", null, "" ], [ "DormancyUtils", "at/schauer/gregor/dormancy/util/DormancyUtils.html", null, "" ], [ "HibernatePropertyAccessor", "at/schauer/gregor/dormancy/util/HibernatePropertyAccessor.html", null, "" ], [ "SpringPropertyAccessor", "at/schauer/gregor/dormancy/util/SpringPropertyAccessor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
