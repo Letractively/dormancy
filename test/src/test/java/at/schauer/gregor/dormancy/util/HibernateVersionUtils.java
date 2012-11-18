922cd0cc93d372cee03dc41843b5673ac9c7161d
schauer.gregor <schauer.gregor@f845d26e-ca1b-6a14-9f41-d569aa8e2ed7>
2012-11-18 12:31:25 +0000

    Wiki

--- test/src/test/java/at/schauer/gregor/dormancy/util/HibernateVersionUtils.java
+++ test/src/test/java/at/schauer/gregor/dormancy/util/HibernateVersionUtils.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2012 Gregor Schauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package at.schauer.gregor.dormancy.util;
+
+import javax.annotation.Nonnull;
 
 /**
  * Utilities for making Hibernate 3 and Hibernate 4 compatible.
@@ -7,13 +24,24 @@ package at.schauer.gregor.dormancy.util;
  * Thus, the following code attempts to load the class regardless of the version present in the classpath.
  *
  * @author Gregor Schauer
+ * @since 1.1.0
  */
 public class HibernateVersionUtils {
 	public static final String HIBERNATE_3_COLLECTION = "org.hibernate.collection.PersistentCollection";
-	public static final String HIBERNATE_4_COLLECTION = "org.hibernate.collection.spi.PersistentCollection";
+	public static final String HIBERNATE_3_LIST = "org.hibernate.collection.PersistentList";
 	public static final String HIBERNATE_3_SET = "org.hibernate.collection.PersistentSet";
-	public static final String HIBERNATE_4_SET = "org.hibernate.collection.internal.PersistentSet";
+	public static final String HIBERNATE_3_MAP = "org.hibernate.collection.PersistentMap";
 
+	public static final String HIBERNATE_4_COLLECTION = "org.hibernate.collection.spi.PersistentCollection";
+	public static final String HIBERNATE_4_LIST = "org.hibernate.collection.internal.PersistentList";
+	public static final String HIBERNATE_4_SET = "org.hibernate.collection.internal.PersistentSet";
+	public static final String HIBERNATE_4_MAP = "org.hibernate.collection.internal.PersistentMap";
+
+	/**
+	 * Returns {@code true} if Hibernate 3 collections are found in the classpath.
+	 *
+	 * @return {@code true} if Hibernate 3 is available, {@code false} otherwise
+	 */
 	public static boolean isHibernate3() {
 		try {
 			return Class.forName(HIBERNATE_3_COLLECTION) != null;
@@ -22,6 +50,11 @@ public class HibernateVersionUtils {
 		}
 	}
 
+	/**
+	 * Returns {@code true} if Hibernate 4 collections are found in the classpath.
+	 *
+	 * @return {@code true} if Hibernate 4 is available, {@code false} otherwise
+	 */
 	public static boolean isHibernate4() {
 		try {
 			return Class.forName(HIBERNATE_4_COLLECTION) != null;
@@ -30,6 +63,12 @@ public class HibernateVersionUtils {
 		}
 	}
 
+	/**
+	 * Returns the class used by Hibernate for representing a persistent collection.
+	 *
+	 * @return the persistent collection class
+	 */
+	@Nonnull
 	public static Class<?> getHibernateCollectionClass() {
 		try {
 			// Attempt to load the PersistentCollection class shipped with Hibernate 4
@@ -45,6 +84,12 @@ public class HibernateVersionUtils {
 		}
 	}
 
+	/**
+	 * Returns the class used by Hibernate for representing a persistent set.
+	 *
+	 * @return the persistent set class
+	 */
+	@Nonnull
 	public static Class<?> getHibernateSetClass() {
 		try {
 			// Attempt to load the PersistentSet class shipped with Hibernate 3
